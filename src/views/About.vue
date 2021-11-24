b7f0ac9db67ab41d111a23409b4c8b57183e7c80
prettywang2002 <shelly.tycg@gmail.com>
2021-11-25 00:34:23 +0800

    安裝黨建立

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,8 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+
+  <div class="bg-purple sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange">
+  Test
+  </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
+<i class="fas fa-align-left"></i>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+
+      <div class="flex justify-evenly p-10 focus-within:bg-green-100 group">
+    <box class="bg-red-500 hover:bg-red-400 ring-red-300 focus:ring-4 group-hover:text-gray-600 font-bold flex justify-center items-center rounded-md cursor-pointer outline-none w-20 h-20 text-3xl text-white" tabindex="0">
+      1
+    </box>
+    <box class="bg-blue-500 hover:bg-blue-400 ring-blue-300 focus:ring-4 group-hover:text-gray-600 font-bold flex justify-center items-center rounded-md cursor-pointer outline-none w-20 h-20 text-3xl text-white" tabindex="1">
+      2
+    </box>
   </div>
 </template>
 
